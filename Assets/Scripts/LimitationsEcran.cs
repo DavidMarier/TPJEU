164df7ef96310ab1a8ec1fda1995a0d5e9291d1d
DavidMarier <144372590+DavidMarier@users.noreply.github.com>
2024-04-21 22:06:57 -0400

    Update LimitationsEcran.cs

--- Assets/Scripts/LimitationsEcran.cs
+++ Assets/Scripts/LimitationsEcran.cs
@@ -13,15 +13,20 @@ public class LimitationsEcran : MonoBehaviour
     void Start(){
         // Prend les dimensions de l'écran
         LimitesEcran = MainCamera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        // Récupère la moitié de la largeur du sprite
         LargeurObjet = transform.GetComponent<SpriteRenderer>().bounds.extents.x;
+        // Récupère la moitié de la hauteur du sprite
         HauteurObjet = transform.GetComponent<SpriteRenderer>().bounds.extents.y; 
     }
 
     // Update is called once per frame
     void LateUpdate(){
-        Vector2 viewPos = transform.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, LimitesEcran.x * -1 + LargeurObjet, LimitesEcran.x - LargeurObjet);
-        viewPos.y = Mathf.Clamp(viewPos.y, LimitesEcran.y * -1 + HauteurObjet, LimitesEcran.y - HauteurObjet);
-        transform.position = viewPos;
+        // Contient la position actuelle de l'objet
+        Vector2 Position = transform.position;
+        // Contient l'objet dans les limites de l'écran
+        Position.x = Mathf.Clamp(Position.x, LimitesEcran.x * -1 + LargeurObjet, LimitesEcran.x - LargeurObjet);
+        Position.y = Mathf.Clamp(Position.y, LimitesEcran.y * -1 + HauteurObjet, LimitesEcran.y - HauteurObjet);
+        // Actualise la position de l'objet
+        transform.position = Position;
     }
 }
